f6ebcd921e0072070e79bdfc149c8832a50bca36
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-16 22:35:07 -0400

    added a foreign key to employee table in order to setup manager name refrences

--- schema.sql
+++ schema.sql
@@ -22,6 +22,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL,
-    PRIMARY KEY (id)
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 );
